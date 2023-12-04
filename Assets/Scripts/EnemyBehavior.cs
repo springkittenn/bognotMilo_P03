c7bac07c89451ff6b45e9da021a9b5b412bcb0e8
springkittenn <joanabognot3@gmail.com>
2023-12-03 23:15:51 -0600

    Cleaning Up EnemyBehavior Script and Unity Editor
    I added tooltips and headers for the Unity editor and also organized the code.

--- Assets/Scripts/EnemyBehavior.cs
+++ Assets/Scripts/EnemyBehavior.cs
@@ -5,23 +5,33 @@ using UnityEngine;
 public class EnemyBehavior : MonoBehaviour
 {
     // PLAYER VAR
+    [Header("Player Stats (No Interaction Needed)")]
+    [Tooltip("Shows if the Player is in the line-of-sight of the Enemy.")]
     public bool playerInRange = false;
     private Transform player;
 
     // IDLE VAR
+    [Header("Idle Stats")]
+    [Range(180, 500)] [Tooltip("Determines how fast the Enemy turns as it looks around. Regular Range: 180-500")]
     public float turningSpeed = 300f;
+    [Range(1, 20)][Tooltip("Determines how far can your Enemy detect the player. Regular Range: 1-20")] 
     public float detectionRange = 8f;
+    [Range(1, 10)][Tooltip("Determines how often your Enemy looks around during idle state. Regular Range: 1-10")] 
     public float lookAroundInterval = 4f;
+
     private float timeSinceLastLook = 0f;
     private bool isLookingAround = false;
-
-    private Quaternion initialRotation;
-    private Quaternion targetRotation;
+    private Quaternion initialRotation, targetRotation;
 
     // ALERT VAR
+    [Header("Alert Stats")]
+    [Range(45, 180)][Tooltip("Determines how wide the Enemy's view angle is to detect the player. Regular Range: 45-180 " +
+        "(Think of it like angle degrees)")] 
     public float fieldOfViewAngle = 90f;
+    [Range(1,5)][Tooltip("Determines how fast the Enemy chases the player once spotted. Remember to consider" +
+        " the Player's movement speed. Regular Range: 1-5")] 
     public float chaseSpeed = 2f;
-    public AudioSource alert;
+    [Tooltip("Assign AudioSource to play an Alert sound when the Player is spotted.")] public AudioSource alertSound;
     private bool playerJustEnteredSight = false;
 
     //-------------------------------------------------------------------------------------------
@@ -34,52 +44,36 @@ public class EnemyBehavior : MonoBehaviour
 
     void Update()
     {
-        // Find the player GameObject (you can use a tag, layer, or other methods)
+        // Find player and position, measure distance between Enemy and Player
         player = GameObject.FindWithTag("Player").transform;
-
-        // Calculate the distance and direction to the player
         float distanceToPlayer = Vector3.Distance(transform.position, player.position);
         Vector3 directionToPlayer = player.position - transform.position;
-
-        // Calculate the angle between the enemy's forward vector and the direction to the player
         float angleToPlayer = Vector3.Angle(transform.forward, directionToPlayer);
 
-        // Check if the player is within the detection range
         if (distanceToPlayer <= detectionRange && angleToPlayer <= fieldOfViewAngle * 0.5f)
         {
-            // Check line of sight
             if (HasLineOfSightToPlayer(directionToPlayer))
             {
                 if (!playerInRange)
                 {
-                    // Player has just entered the line of sight
                     playerJustEnteredSight = true;
                 }
 
                 playerInRange = true;
-
+                isLookingAround = false;
                 directionToPlayer.y = 0;
 
-                // Create a rotation that looks at the player
                 targetRotation = Quaternion.LookRotation(directionToPlayer);
-
-                // Reset look around state
-                isLookingAround = false;
-
-                // Move the enemy towards the player
                 transform.position = Vector3.MoveTowards(transform.position, player.position, chaseSpeed * Time.deltaTime);
-
-                // Update rotation to look at the player
                 targetRotation = Quaternion.LookRotation(player.position - transform.position);
                 float step = turningSpeed * Time.deltaTime;
                 transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, step);
 
                 if (playerJustEnteredSight)
                 {
-                    alert.Play();
-                    playerJustEnteredSight = false; // Reset the flag
+                    alertSound.Play();
+                    playerJustEnteredSight = false; // Reset
                 }
-
                 else
                 {
                     isLookingAround = true;
@@ -89,20 +83,18 @@ public class EnemyBehavior : MonoBehaviour
         else
         {
             playerInRange = false;
-
             isLookingAround = true;
 
-            // While the player is not in range, make the enemy look around gradually
             timeSinceLastLook += Time.deltaTime;
 
             if (timeSinceLastLook >= lookAroundInterval)
             {
                 StartLookingAround();
-                timeSinceLastLook = 0f; // Reset the timer
+                timeSinceLastLook = 0f; // Reset
             }
         }
 
-        // Update rotation gradually with a faster turning speed
+        // Smooth rotations
         if (isLookingAround)
         {
             float step = turningSpeed * Time.deltaTime;
@@ -110,15 +102,15 @@ public class EnemyBehavior : MonoBehaviour
         }
         else
         {
-            // Always face the player if in range
             float step = turningSpeed * Time.deltaTime;
             transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, step);
         }
     }
 
+    //-------------------------------------------------------------------------------------------
+
     void StartLookingAround()
     {
-        // Randomly set the target rotation for looking around
         float randomYRotation = Random.Range(0f, 360f);
         targetRotation = initialRotation * Quaternion.Euler(0, randomYRotation, 0);
         isLookingAround = true;
@@ -126,23 +118,17 @@ public class EnemyBehavior : MonoBehaviour
 
     bool HasLineOfSightToPlayer(Vector3 directionToPlayer)
     {
-        // Cast a ray from the enemy towards the player
         RaycastHit hit;
-
-        // Draw a red line in the Scene view to visualize the raycast (extend to the detection range)
         Debug.DrawRay(transform.position, directionToPlayer.normalized * detectionRange, Color.red);
 
         if (Physics.Raycast(transform.position, directionToPlayer, out hit, detectionRange))
         {
-            // Check if the hit object is the player
             if (hit.collider.CompareTag("Player"))
             {
-                // Draw a green line in the Scene view to visualize the raycast
                 Debug.DrawRay(transform.position, directionToPlayer.normalized * detectionRange, Color.green);
-
-                return true; // Player is in line of sight
+                return true;
             }
         }
-        return false; // No line of sight
+        return false;
     }
 }
